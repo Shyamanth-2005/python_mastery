10803068907ee69f93d6227ba6e3472395029c36
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-16 15:07:11 +0530

    import functions from my_module and renaming the function

--- 12_day/main.py
+++ 12_day/main.py
@@ -1,6 +1,6 @@
-from my_module import generate_full_name,two_sum
+from my_module import generate_full_name as fn, two_sum as ts
 
-if __name__ == "__main__":
-  print(generate_full_name("shyamanth","reddy"))
-  print(two_sum(12,21))
+if __name__ == '__main__':
+  print(fn("shyamanth","reddy"))
   
+  print(ts(75,213))
