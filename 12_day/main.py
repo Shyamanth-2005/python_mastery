94aea9a972da3b92462af81d2ec491bfaa6a0961
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-16 15:16:21 +0530

    tried os module

--- 12_day/main.py
+++ 12_day/main.py
@@ -1,6 +1,15 @@
-from my_module import generate_full_name as fn, two_sum as ts
+# from my_module import generate_full_name as fn, two_sum as ts
 
-if __name__ == '__main__':
-  print(fn("shyamanth","reddy"))
+# if __name__ == '__main__':
+#   print(fn("shyamanth","reddy"))
   
-  print(ts(75,213))
+#   print(ts(75,213))
+
+# import built-in  modules
+
+import os
+def using_os_built_in():
+  print(os.getcwd())
+
+  PATH = r"C:\Users\Shyam\Desktop\python_learning\13_day"
+  os.mkdir(path=PATH)
