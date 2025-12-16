57b5cf9aff9370e91348f47369fdc34fe55bc621
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-16 15:00:45 +0530

    my_module is being imported using import keyword

---
+++ 12_day/main.py
@@ -0,0 +1,5 @@
+import my_module
+
+if __name__ == "__main__":
+  print(my_module.generate_full_name("Shyamanth","Reddy"))
+  
---
+++ 12_day/my_module.py
@@ -0,0 +1,5 @@
+def generate_full_name(first_name,last_name):
+  return first_name + " " + last_name
+
+def two_sum(number_one,number_two):
+  return number_one + number_two
