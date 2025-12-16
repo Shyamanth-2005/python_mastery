07394f51c668be91c2f491052ae6e9753b6c6a5c
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-16 17:42:48 +0530

    worked on statistics module

---
+++ 12_day/statistic_module.py
@@ -0,0 +1,8 @@
+from statistics import *  # importing all the statistics modules
+
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+print(mean(ages))
+print(median(ages))
+print(mode(ages))
+print(stdev(ages))
+
