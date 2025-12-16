c8a47553a78d9baefc1e4feabe086868853cf000
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-16 17:50:53 +0530

    worked on inbuilt math module

---
+++ 12_day/math_module.py
@@ -0,0 +1,8 @@
+import math
+
+print(math.pi)
+print(math.sqrt(2))
+print(math.pow(2,3)) # 8.0
+print(math.floor(9.81)) # 9 floor -> will increase the rounding to the lowest nearest number
+print(math.ceil(9.81)) # 10 ceil -> will increase the rounding to the highest nearest number
+print(math.log10(100)) # 2 
