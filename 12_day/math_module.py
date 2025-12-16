9e426ae129681f428a2d73a9722db2a6824cfc51
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-16 17:57:17 +0530

    added the different importing methods

--- 12_day/math_module.py
+++ 12_day/math_module.py
@@ -5,4 +5,18 @@ print(math.sqrt(2))
 print(math.pow(2,3)) # 8.0
 print(math.floor(9.81)) # 9 floor -> will increase the rounding to the lowest nearest number
 print(math.ceil(9.81)) # 10 ceil -> will increase the rounding to the highest nearest number
-print(math.log10(100)) # 2 
+print(math.log10(100)) # 2
+
+
+
+"""
+We can write the import statement  in the following ways
+
+from math import pi
+
+from math import pi,sqrt,pow,floor ,ceil , log10
+
+from math import * => get all the functions from the module
+
+from math import pi as PI => renaming the pi function as PI
+"""
